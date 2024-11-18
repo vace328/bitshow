073040cc7c4694e8273efbbaee319cb1cefb6fa8
anaz <zlatkovic.ana@gmail.com>
2024-11-18 12:54:50 +0100

    landing page - images as tags, not bg

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-// content display
-// const URL = "https://api.tvmaze.com/shows";
 const display = document.querySelector("#content-wrapper");
-// const search = document.querySelector("#search");
-// const searchResults = document.querySelector("#search-results");
 
 function getShows(url) {
   fetch(url)
@@ -31,10 +27,11 @@ function displayItem(data) {
 
     // thumbnail
     const imgWrapper = document.createElement("div");
-    const thumb = `${show.image.original}`;
-    // console.log(thumb);
+    const thumb = `${show.image.medium}`;
+    const img = document.createElement("img");
     imgWrapper.classList.add("thumbnail");
-    imgWrapper.style.backgroundImage = `url(${thumb})`;
+    img.src = thumb;
+    imgWrapper.append(img);
     showCard.append(imgWrapper);
 
     // title
@@ -50,64 +47,6 @@ function displayItem(data) {
   });
 }
 
-// // FILTER SHOWS
-// function filterShows(query, container) {
-//   const searchUrl = `https://api.tvmaze.com/search/shows?q=${query}`;
-//   fetch(searchUrl)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       displayFilteredData(data, container);
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// function displayFilteredData(data, container) {
-//   const firstTen = data?.slice(0, 10);
-//   if (firstTen.length > 0) {
-//     container.style.display = "block";
-//     container.innerHTML = "";
-//     firstTen.forEach((showItem) => {
-//       const result = document.createElement("p");
-//       result.id = showItem?.show?.id;
-//       result.innerText = showItem?.show?.name;
-//       container.append(result);
-//       result.addEventListener("click", () => {
-//         getSingleShow(URL, result.id, container);
-//       });
-//     });
-//   }
-//   console.log(firstTen);
-// }
-
-// function getSingleShow(baseURL, id, container) {
-//   const url = `${baseURL}/${id}`;
-//   console.log(url);
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       dispalySingleShowPage(data, container);
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// function dispalySingleShowPage(data, searchResultsContainer) {
-//   if (searchResultsContainer) {
-//     searchResultsContainer.style.display = "none";
-//   }
-//   // console.log(data);
-//   localStorage.setItem("showDetails", JSON.stringify(data));
-//   window.open("/single-show/single-show.html", "_self");
-// }
-
-// search.addEventListener("keyup", () => {
-//   if (search.value.length === 0) {
-//     searchResults.style.display = "none";
-//   } else {
-//     console.log(search.value);
-
-//     filterShows(search.value, searchResults);
-//   }
-// });
 window.addEventListener("load", () => {
   getShows(URL);
 });
